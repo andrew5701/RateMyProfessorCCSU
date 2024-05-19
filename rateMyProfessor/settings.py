b6c30d150cf10a6c70ae1c90abbfe32136a4b134
Andrew <andrew76280@gmail.com>
2024-05-19 18:26:51 -0400

    feat: Replaced files with files from newer version

--- rateMyProfessor/settings.py
+++ rateMyProfessor/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig', #our base folder
-    'accounts'
+    'accounts',
+    'profanity',
 
 ]
 
